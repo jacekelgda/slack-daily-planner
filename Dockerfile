10f967bbe3b6d3b48e77373879cb29b1809dadf2
Jacek Ławniczak <jacekelgda@gmail.com>
2017-03-24 15:34:11 +0100

    Generating list based on direct message and providing interactive message

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,9 @@
 FROM node:7
 
-RUN npm install supervisor -g
-
 RUN mkdir /src
 
 WORKDIR /src
 ADD package.json /src/package.json
 RUN npm install
 
-CMD supervisor -w app app/app.js
+CMD npm start
--- app/app.js
+++ app/app.js
@@ -1,21 +1,93 @@
-var request = require('request');
 var Botkit = require('botkit');
+var cron = require('node-cron');
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
     process.exit(1);
 }
 
-var controller = Botkit.slackbot({
+var listener = Botkit.slackbot({
     debug: true,
     stats_optout: true
 });
 
-var bot = controller.spawn({
+var bot = listener.spawn({
     token: process.env.token
 }).startRTM();
 
-controller.hears('hello', 'direct_message', function(bot, message) {
-    bot.reply(message, 'Hi!');
-  }
-);
+// responding to direct messages
+listener.on('direct_message', (bot, message) => {
+  const itemsString = message.text;
+  const items = itemsString.split(";").map((item) => {
+    return item.trim();
+  })
+  const list = generateList(items);
+  sendGeneratedListForApproval(list);
+})
+// responding to direct messages <end>
+
+// generate list
+function generateList(items) {
+  const list = items.map((item) => {
+    return '[ ] ' + item + '\n';
+  })
+
+  return list;
+}
+
+function formatListToSlackText(list) {
+  let listAsText = '';
+  list.forEach((item, index) => {
+    listAsText += item;
+  });
+
+  return listAsText;
+}
+// generate list <end>
+
+function sendGeneratedListForApproval(list) {
+  bot.startPrivateConversation({user:process.env.slack_user}, (err, convo) => {
+    console.log(process.env.slack_user);
+    convo.say({
+        attachments:[
+            {
+                title: 'Do you want to publish this list to your journal?',
+                text: formatListToSlackText(list),
+                callback_id: '123',
+                attachment_type: 'default',
+                actions: [
+                    {
+                        "name":"yes",
+                        "text": "Yes",
+                        "value": "yes",
+                        "type": "button",
+                    },
+                    {
+                        "name":"no",
+                        "text": "No",
+                        "value": "no",
+                        "type": "button",
+                    }
+                ]
+            }
+        ]
+    });
+  });
+}
+
+// starting conversation
+function setupCron() {
+  cron.schedule('* * * * *', function(){
+    privateConversation();
+  });
+};
+
+function privateConversation() {
+  bot.startPrivateConversation({user:process.env.slack_user}, (err, convo) => {
+    convo.ask('Whats your plan for today?', (response, convo) => {
+      convo.say('Cool, you said: ' + response.text);
+      convo.next();
+    });
+  });
+};
+// starting conversation <end>
