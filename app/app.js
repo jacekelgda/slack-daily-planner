5a7fc1bbc383e18a1837652b811d498eb2020752
Jacek Ławniczak <jacekelgda@gmail.com>
2017-03-24 16:42:04 +0100

    Added firebase and db structure for initial lists

--- app/app.js
+++ app/app.js
@@ -1,5 +1,6 @@
-var Botkit = require('botkit');
-var cron = require('node-cron');
+const Botkit = require('botkit');
+const cron = require('node-cron');
+const firebase = require('firebase');
 
 if (!process.env.token) {
     console.log('Error: Specify token in environment');
@@ -15,15 +16,38 @@ var bot = listener.spawn({
     token: process.env.token
 }).startRTM();
 
+const config = {
+  apiKey: "AIzaSyD0BSlwscNQdUlkKkDN96nk8NUt7jIHd_M",
+  authDomain: "todos-16130.firebaseapp.com",
+  databaseURL: "https://todos-16130.firebaseio.com",
+  storageBucket: "todos-16130.appspot.com",
+  messagingSenderId: "1033025372827"
+};
+const app = firebase.initializeApp(config);
+
+// start
+setupCron();
+// start <end>
+
 // responding to direct messages
 listener.on('direct_message', (bot, message) => {
+  handleMessage(message);
+})
+
+function handleMessage(message) {
+  const items = processMessage(message);
+  const list = generateList(items);
+  sendGeneratedListForApproval(list);
+}
+
+function processMessage(message) {
   const itemsString = message.text;
   const items = itemsString.split(";").map((item) => {
     return item.trim();
-  })
-  const list = generateList(items);
-  sendGeneratedListForApproval(list);
-})
+  });
+
+  return items;
+}
 // responding to direct messages <end>
 
 // generate list
@@ -45,47 +69,72 @@ function formatListToSlackText(list) {
 }
 // generate list <end>
 
-function sendGeneratedListForApproval(list) {
-  bot.startPrivateConversation({user:process.env.slack_user}, (err, convo) => {
-    console.log(process.env.slack_user);
-    convo.say({
-        attachments:[
-            {
-                title: 'Do you want to publish this list to your journal?',
-                text: formatListToSlackText(list),
-                callback_id: '123',
-                attachment_type: 'default',
-                actions: [
-                    {
-                        "name":"yes",
-                        "text": "Yes",
-                        "value": "yes",
-                        "type": "button",
-                    },
-                    {
-                        "name":"no",
-                        "text": "No",
-                        "value": "no",
-                        "type": "button",
-                    }
-                ]
-            }
-        ]
-    });
+function sendGeneratedListForApproval(list, convo) {
+  if (convo) {
+    askWithInteractiveMessage(list, convo);
+  } else {
+    sendInteractiveMessageAsNewConversation(list);
+  }
+}
+
+function askWithInteractiveMessage(list, convo) {
+  convo.ask({
+      attachments:[
+          {
+              title: 'Do you want to publish this list to your journal?',
+              text: formatListToSlackText(list),
+              callback_id: '123',
+              attachment_type: 'default',
+              actions: [
+                  {
+                      "name":"yes",
+                      "text": "Yes",
+                      "value": "yes",
+                      "type": "button",
+                  },
+                  {
+                      "name":"no",
+                      "text": "No",
+                      "value": "no",
+                      "type": "button",
+                  }
+              ]
+          }
+      ]
   });
+}
+
+function sendInteractiveMessageAsNewConversation(list) {
+  bot.startPrivateConversation({user:process.env.slack_user}, (err, convo) => {
+    askWithInteractiveMessage(list, convo);
+  });
+}
+
+
+function persistInitialListTasks(id, message) {
+  const items = processMessage(message);
+  items.forEach((item, index) => {
+    firebase.database().ref('lists/' + id + '/tasks/' + index).set({
+      name: item,
+      achieved: false
+    });
+  })
 }
 
 // starting conversation
 function setupCron() {
-  cron.schedule('* * * * *', function(){
-    privateConversation();
+  privateConversation(Date.now());
+  cron.schedule('* * * * *', () => {
+    privateConversation(Date.now());
   });
 };
 
-function privateConversation() {
+function privateConversation(id) {
   bot.startPrivateConversation({user:process.env.slack_user}, (err, convo) => {
-    convo.ask('Whats your plan for today?', (response, convo) => {
-      convo.say('Cool, you said: ' + response.text);
+    convo.ask('Whats your plan for today?', (message, convo) => {
+      console.log('updating list/' + id);
+      persistInitialListTasks(id, message);
+      convo.say('Cool, you said: ' + message.text);
       convo.next();
     });
   });
